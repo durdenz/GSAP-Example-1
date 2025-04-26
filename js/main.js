99954b58363d7f0e8b71a700a5ecaaff58e69383
George Durden <george@durden.com>
2025-04-26 07:39:04 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+ document.addEventListener("DOMContentLoaded", (event) => {
+  // gsap code here!
+    gsap.to(".box", {
+    rotation : 360,
+    duration : 2,
+    repeat: -1,
+    repeatDelay: .25,
+    })
+ });
+
+
+
+
